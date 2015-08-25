ed3bc8348ceb7a32b7475ffdec8fe1f6d80f7011
Minerva Archer <minerva.archer@gmail.com>
2015-08-25 18:05:09 -0400

    Initial commit: "Developing with Webpack" chapter through section "Refreshing CSS"

---
+++ README.md
@@ -0,0 +1,3 @@
+# Survive JS
+
+My attempt to learn Webpack + React using the [Survive.js](http://survivejs.com) ebook.
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  entry: path.resolve(ROOT_PATH, 'app/main'),
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  },
+  devServer: {
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    port: 4000
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
