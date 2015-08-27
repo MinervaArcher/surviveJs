f743d32644f0191102eef11b89834c544e8a5b4c
Minerva Archer <minerva.archer@gmail.com>
2015-08-27 13:14:44 -0400

    Finished "Building Kanban" chapter. <3 u, Webpack.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,28 +1,25 @@
+var fs = require('fs');
+var React = require('react');
 var path = require('path');
-var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
 var Clean = require('clean-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var App = require('./app/components/App.jsx');
 var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
-
 
 var common = {
   entry: path.resolve(ROOT_PATH, 'app/main.jsx'),
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
-  },
-  plugins: [
-    new HtmlwebpackPlugin({
-      title: 'Kanban app'
-    })
-  ]
-}
+  }
+};
 
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
@@ -31,8 +28,7 @@ if(TARGET === 'start' || !TARGET) {
       loaders: [
         {
           test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: path.resolve(ROOT_PATH, 'app')
+          loaders: ['style', 'css']
         },
         {
           test: /\.jsx?$/,
@@ -46,11 +42,13 @@ if(TARGET === 'start' || !TARGET) {
       historyApiFallback: true,
       hot: true,
       inline: true,
-      progress: true,
-      port: 4000
+      progress: true
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlwebpackPlugin({
+        title: 'Kanban app'
+      })
     ]
   });
 }
@@ -81,23 +79,41 @@ if(TARGET === 'build') {
       ]
     },
     plugins: [
-    new ExtractTextPlugin('styles.css'),
-    new Clean(['build']),
-    new webpack.optimize.CommonsChunkPlugin(
-      'vendor',
-      'vendor.[chunkhash].js'
-    ),
-    new webpack.DefinePlugin({
-      'process.env': {
-        // This affects react lib size
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
+      new ExtractTextPlugin('styles.css'),
+      new Clean(['build']),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        'vendor.[chunkhash].js'
+      ),
+      new webpack.DefinePlugin({
+        'process.env': {
+          // This affects react lib size
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
         }
+      }),
+      new HtmlwebpackPlugin({
+        title: 'Kanban app',
+        templateContent: renderTemplate(
+          fs.readFileSync(path.join(__dirname, 'templates/index.tpl'), 'utf8'),
+          {
+            app: React.renderToString(<App />)
+          })
       })
     ]
   });
+}
+
+function renderTemplate(template, replacements) {
+  return function() {
+    return template.replace(/%(\w*)%/g, function(match) {
+      var key = match.slice(1, -1);
+
+      return replacements[key] ? replacements[key] : match;
+    });
+  };
 }
