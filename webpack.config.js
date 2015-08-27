85e55914b7d133a32989d9335e456b24db8f9b67
Minerva Archer <minerva.archer@gmail.com>
2015-08-27 12:51:13 -0400

    "Building Kanban" chapter, through "Separating CSS"

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
+var Clean = require('clean-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
@@ -12,15 +16,6 @@ var common = {
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        include: path.resolve(ROOT_PATH, 'app')
-      }
-    ]
   },
   plugins: [
     new HtmlwebpackPlugin({
@@ -34,6 +29,11 @@ if(TARGET === 'start' || !TARGET) {
     devtool: 'eval-source-map',
     module: {
       loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: path.resolve(ROOT_PATH, 'app')
+        },
         {
           test: /\.jsx?$/,
           loaders: ['react-hot', 'babel'],
@@ -51,6 +51,53 @@ if(TARGET === 'start' || !TARGET) {
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    entry: {
+      app: path.resolve(ROOT_PATH, 'app/main.jsx'),
+      vendor: Object.keys(pkg.dependencies)
+    },
+    output: {
+      path: path.resolve(ROOT_PATH, 'build'),
+      filename: 'app.[chunkhash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: path.resolve(ROOT_PATH, 'app')
+        },
+        {
+          test: /\.jsx?$/,
+          loaders: ['babel'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    },
+    plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new Clean(['build']),
+    new webpack.optimize.CommonsChunkPlugin(
+      'vendor',
+      'vendor.[chunkhash].js'
+    ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        // This affects react lib size
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
     ]
   });
 }
